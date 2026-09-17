9c000cec6ab1d8d06f5bc5e7baa7037715e18eeb
Edgar Villegas <narnagas@gmail.com>
2026-09-17 01:34:06 -0500

    feat: detect players entering lobby

--- horizon-worlds/lobby/scripts/122373191516000587/scripts/lobbyController.ts
+++ horizon-worlds/lobby/scripts/122373191516000587/scripts/lobbyController.ts
@@ -4,7 +4,16 @@ class lobbyController extends hz.Component<typeof lobbyController> {
   static propsDefinition = {};
 
   start() {
-
+    this.connectCodeBlockEvent(
+      this.entity,
+      hz.CodeBlockEvents.OnPlayerEnterWorld,
+      this.onPlayerEnterWorld,
+    );
   }
+
+  private onPlayerEnterWorld = (player: hz.Player) => {
+    console.log(`Player entered the lobby: ${player.id}`);
+  };
 }
+
 hz.Component.register(lobbyController);
