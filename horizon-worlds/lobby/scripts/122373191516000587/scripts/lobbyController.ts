abad26b4485ff2ca4008c168d74bbbc4ab13c330
Edgar Villegas <narnagas@gmail.com>
2026-09-17 17:11:46 -0500

    feat: sequence lobby main and accent lights

--- horizon-worlds/lobby/scripts/122373191516000587/scripts/lobbyController.ts
+++ horizon-worlds/lobby/scripts/122373191516000587/scripts/lobbyController.ts
@@ -19,6 +19,16 @@ class lobbyController extends hz.Component<typeof lobbyController> {
       console.warn('[Lobby] Main light is not assigned');
     }
 
+    if (this.props.accentLight) {
+      const accentLight = this.props.accentLight.as(hz.DynamicLightGizmo);
+
+      console.log('[Lobby] Accent light assigned');
+      accentLight.enabled.set(false);
+      console.log('[Lobby] Accent light disabled');
+    } else {
+      console.warn('[Lobby] Accent light is not assigned');
+    }
+
     this.connectCodeBlockEvent(
       this.entity,
       hz.CodeBlockEvents.OnPlayerEnterWorld,
@@ -38,6 +48,15 @@ class lobbyController extends hz.Component<typeof lobbyController> {
         console.log('[Lobby] Main light enabled');
       }
     }, 1500);
+
+    this.async.setTimeout(() => {
+      if (this.props.accentLight) {
+        const accentLight = this.props.accentLight.as(hz.DynamicLightGizmo);
+
+        accentLight.enabled.set(true);
+        console.log('[Lobby] Accent light enabled');
+      }
+    }, 2500);
   };
 }
 
