999d8c74840d2ccb10f5f4ceda057ec1ecc79e3f
Edgar Villegas <edgar.villegas@lalagunatek.com>
2026-09-17 01:30:33 -0500

    chore: initialize Horizon Worlds lobby script workspace

---
+++ horizon-worlds/lobby/scripts/122373191516000587/scripts/lobbyController.ts
@@ -0,0 +1,10 @@
+import * as hz from 'horizon/core';
+
+class lobbyController extends hz.Component<typeof lobbyController> {
+  static propsDefinition = {};
+
+  start() {
+
+  }
+}
+hz.Component.register(lobbyController);
